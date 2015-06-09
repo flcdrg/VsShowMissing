5f566ca09c43cebd68e52ec3b40c0da99c4d2cd2
David Gardiner <david@gardiner.net.au>
2015-06-09 16:11:42 +0930

    Remove menu

---
+++ Gardiner.VsShowMissing/Gardiner.VsShowMissingPackage.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.ComponentModel.Design;
+using System.IO;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft.Win32;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+
+namespace DavidGardiner.Gardiner_VsShowMissing
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    ///
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the
+    /// IVsPackage interface and uses the registration attributes defined in the framework to
+    /// register itself and its components with the shell.
+    /// </summary>
+    // This attribute tells the PkgDef creation utility (CreatePkgDef.exe) that this class is
+    // a package.
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    // This attribute is used to register the information needed to show this package
+    // in the Help/About dialog of Visual Studio.
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    // This attribute is needed to let the shell know that this package exposes some menus.
+    //[ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(GuidList.guidGardiner_VsShowMissingPkgString)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExists_string)]
+    public sealed class Gardiner_VsShowMissingPackage : Package, IVsSolutionEvents
+    {
+        private DTE _dte;
+        private uint _solutionCookie;
+        private IVsSolution _solution;
+
+        /// <summary>
+        /// Default constructor of the package.
+        /// Inside this method you can place any initialization code that does not require
+        /// any Visual Studio service because at this point the package object is created but
+        /// not sited yet inside Visual Studio environment. The place to do all the other
+        /// initialization is the Initialize method.
+        /// </summary>
+        public Gardiner_VsShowMissingPackage()
+        {
+            Debug.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering constructor for: {0}", this.ToString()));
+        }
+
+
+
+        /////////////////////////////////////////////////////////////////////////////
+        // Overridden Package Implementation
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        protected override void Initialize()
+        {
+            Debug.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering Initialize() of: {0}", this.ToString()));
+            base.Initialize();
+
+            // Add our command handlers for menu (commands must exist in the .vsct file)
+/*            OleMenuCommandService mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (null != mcs)
+            {
+                // Create the command for the menu item.
+                CommandID menuCommandID = new CommandID(GuidList.guidGardiner_VsShowMissingCmdSet, (int)PkgCmdIDList.cmdidFindMissingFiles);
+                MenuCommand menuItem = new MenuCommand(MenuItemCallback, menuCommandID);
+                mcs.AddCommand(menuItem);
+            }*/
+
+            // listen for solution events
+            _solution = (IVsSolution)GetService(typeof(SVsSolution));
+            ErrorHandler.ThrowOnFailure(_solution.AdviseSolutionEvents(this, out _solutionCookie));
+
+            _dte = (DTE)GetService(typeof(SDTE));
+            var events = _dte.Events;
+            var buildEvents = events.BuildEvents;
+
+            buildEvents.OnBuildProjConfigBegin += BuildEventsOnOnBuildProjConfigBegin;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_solutionCookie != 0)
+            {
+                _solution.UnadviseSolutionEvents(_solutionCookie);
+                _solutionCookie = 0;
+            }
+
+            base.Dispose(disposing);
+        }
+
+        private void BuildEventsOnOnBuildProjConfigBegin(string project, string projectConfig, string platform, string solutionConfig)
+        {
+            var errorListProvider = new ErrorListProvider(this);
+
+            errorListProvider.Tasks.Clear();
+
+            foreach (Project proj in _dte.Solution.Projects)
+            {
+                foreach (ProjectItem item in proj.ProjectItems)
+                {
+                    if (item.Kind != "{6BB5F8EE-4483-11D3-8BCF-00C04F8EC28C}")
+                        continue;
+
+                    for (short i = 0; i < item.FileCount; i++)
+                    {
+                        var path = item.FileNames[i];
+                        if (!File.Exists(path))
+                        {
+                            // WriteToBuildWindow(string.Format("Missing file: {0} in project {1}", path, proj.Name));
+                            IVsHierarchy heirarchyItem;
+                            _solution.GetProjectOfUniqueName(proj.FileName, out heirarchyItem);
+
+                            var newError = new ErrorTask()
+                            {
+                                ErrorCategory = TaskErrorCategory.Error,
+                                Category = TaskCategory.BuildCompile,
+                                Text = "Missing file",
+                                Document = path,
+                                HierarchyItem = heirarchyItem
+                            };
+
+                            errorListProvider.Tasks.Add(newError);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void WriteToBuildWindow(string message)
+        {
+            var outputWindow = (OutputWindow)_dte.Windows.Item(EnvDTE.Constants.vsWindowKindOutput).Object;
+            var build = outputWindow.OutputWindowPanes.Item("Build");
+            build.OutputString(message + Environment.NewLine);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// This function is the callback used to execute a command when the a menu item is clicked.
+        /// See the Initialize method to see how the menu item is associated to this function using
+        /// the OleMenuCommandService service and the MenuCommand class.
+        /// </summary>
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            // Show a Message Box to prove we were here
+            IVsUIShell uiShell = (IVsUIShell)GetService(typeof(SVsUIShell));
+            Guid clsid = Guid.Empty;
+            int result;
+            Microsoft.VisualStudio.ErrorHandler.ThrowOnFailure(uiShell.ShowMessageBox(
+                       0,
+                       ref clsid,
+                       "Gardiner.VsShowMissing",
+                       string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.ToString()),
+                       string.Empty,
+                       0,
+                       OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                       OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST,
+                       OLEMSGICON.OLEMSGICON_INFO,
+                       0,        // false
+                       out result));
+        }
+
+        public int OnAfterOpenProject(IVsHierarchy pHierarchy, int fAdded)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnQueryCloseProject(IVsHierarchy pHierarchy, int fRemoving, ref int pfCancel)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnBeforeCloseProject(IVsHierarchy pHierarchy, int fRemoved)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnAfterLoadProject(IVsHierarchy pStubHierarchy, IVsHierarchy pRealHierarchy)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnQueryUnloadProject(IVsHierarchy pRealHierarchy, ref int pfCancel)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnBeforeUnloadProject(IVsHierarchy pRealHierarchy, IVsHierarchy pStubHierarchy)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnAfterOpenSolution(object pUnkReserved, int fNewSolution)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnQueryCloseSolution(object pUnkReserved, ref int pfCancel)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnBeforeCloseSolution(object pUnkReserved)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnAfterCloseSolution(object pUnkReserved)
+        {
+            return VSConstants.S_OK;
+        }
+    }
+}
