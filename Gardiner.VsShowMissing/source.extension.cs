5d1c8b52173309e1e3acec08f1ac3534600e9ae1
David Gardiner <david@gardiner.net.au>
2016-09-09 09:07:17 +0930

    fix string

--- Gardiner.VsShowMissing/source.extension.cs
+++ Gardiner.VsShowMissing/source.extension.cs
@@ -9,7 +9,7 @@ namespace DavidGardiner.Gardiner_VsShowMissing
     {
         public const string Id = "36b8e40e-ceaa-4305-bdb7-1a68a0d84067";
         public const string Name = "Show Missing Files";
-        public const string Description = "Find files references in project files that don't exist on disk, and optionally files that do exist on disk that aren't included in a project. 
+        public const string Description = @"Find files references in project files that don't exist on disk, and optionally files that do exist on disk that aren't included in a project. 
 Runs on Build. Missing files are listed in Error List window.";
         public const string Language = "en-US";
         public const string Version = "2.3";
