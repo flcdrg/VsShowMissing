8d19397dc558c5f7ccf18b48a774cf5b06ede4ae
David Gardiner <david@gardiner.net.au>
2019-05-22 20:43:59 +0930

    Mostly working

--- VS2019/source.extension.cs
+++ VS2019/source.extension.cs
@@ -8,7 +8,7 @@ namespace DavidGardiner.Gardiner_VsShowMissing
     internal sealed partial class Vsix
     {
         public const string Id = "VS2019.52b01d49-2718-47a2-a08f-117f19edf495";
-        public const string Name = "Show Missing Files";
+        public const string Name = "Show Missing Files 2019";
         public const string Description = @"Find files references in project files that don't exist on disk, and optionally files that do exist on disk that aren't included in a project. 
 Runs on Build. Missing files are listed in Error List window.";
         public const string Language = "en-US";
